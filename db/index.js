e8051fe3fa00b78c0e531a024a07f30e57107051
mojib2014 <mojib_monir@yahoo.com>
2021-07-13 22:27:26 -0700

    aprepare for deployment to heroku

--- db/index.js
+++ db/index.js
@@ -2,13 +2,21 @@
 const config = require("config");
 const { Pool } = require("pg");
 
-const pool = new Pool({
+const devConfig = {
   user: config.get("db.dbUser"),
   password: config.get("db.dbPassword"),
   database: config.get("db.dbDatabase"),
   host: config.get("db.dbHost"),
   port: config.get("db.dbPort"),
-});
+};
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, // heroku addon
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig,
+);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
